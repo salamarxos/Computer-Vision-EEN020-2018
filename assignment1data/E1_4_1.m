1d21cc8def84f2ac7b9af99dc41afb827188a819
unknown <pipi_karipi@hotmail.com>
2018-11-19 10:51:35 +0100

    uptodate

--- assignment1data/E1_4_1.m
+++ assignment1data/E1_4_1.m
@@ -2,7 +2,7 @@ clc
 clear all 
 close all
 
-%% X1 and X2 transdormation %%
+%% X1 and X2 transdormation 
 H = [1 1 0; 0 1 0; -1 0 1];
 x1 = [1;0; 1]; x2 = [0; 1; 1];
 
@@ -11,8 +11,8 @@ y1 = H*x1; y2 = H*x2;
 %y1(3)=1;
 %% Lines that pass through the points of interset %%
 L1 = pflat(cross(x1,x2));
-L2 = cross(y1,y2);
-
+L2 =pflat(cross(y1,y2));
+L3 = pflat(inv(H)'*L1);
 figure(1)
 markerSize = 6;
 hold on
@@ -20,7 +20,7 @@ plot(x1(1),x1(2),'o','MarkerSize',markerSize,'MarkerEdgeColor','blue',...
     'MarkerFaceColor','blue')
 plot(x2(1),x2(2),'o','MarkerSize',markerSize,'MarkerEdgeColor','blue',...
     'MarkerFaceColor','blue')
-rital(L1);
+plot([x2(1,:); x1(1,:)],[x2(2,:); x1(2,:)],'b-');
 xlim([-1 2])
 ylim([-1 2])
 
@@ -31,6 +31,13 @@ plot(y1(1),y1(2),'o','MarkerSize',markerSize,'MarkerEdgeColor','red',...
     'MarkerFaceColor','red')
 plot(y2(1),y2(2),'o','MarkerSize',markerSize,'MarkerEdgeColor','red',...
     'MarkerFaceColor','red')
-rital(L2);
+plot([y2(1,:); y1(1,:)],[y2(2,:); y1(2,:)],'b-');
 xlim([-1 2])
 ylim([-1 2])
+
+% figure(3)
+% L1_x = L1(1)*linspace(0,1,100);
+% L1_y = L1(2)*linspace(0,1,100);
+% L1_z = L1(3)*linspace(0,1,100);
+% 
+% scatter3([y1(1);y2(1)],[y1(2);y2(2)],[y1(3);y2(3)])
--- assignment1data/E1_4_2.m
+++ assignment1data/E1_4_2.m
@@ -1,4 +1,4 @@
-clc
+    clc
 clear all 
 close all
 
@@ -6,7 +6,7 @@ load('compEx3.mat');
 H1 = [sqrt(3) -1 1; 1 sqrt(3) 1; 0 0 2];
 H2 = [1 -1 1; 1 1 0; 0 0 1];
 H3 = [1 1 0; 0 2 0; 0 0 1];
-H4 = [sqrt(3) -1 1; 1 sqrt(3) 1; 1/4 1/2 2];
+H4 = [sqrt(3) -1 1; 1 sqrt(3) 1; 4 1/2 2];
 
 start1 = (H1*[startpoints; ones(1,size(startpoints,2))]);
 end1 = (H1*[endpoints; ones(1,size(endpoints,2))]);
@@ -16,25 +16,30 @@ start3 = (H3*[startpoints; ones(1,size(startpoints,2))]);
 end3 = (H3*[endpoints; ones(1,size(endpoints,2))]);
 start4 = (H4*[startpoints; ones(1,size(startpoints,2))]);
 end4 = (H4*[endpoints; ones(1,size(endpoints,2))]);
-hold on
-subplot(2,4,[1 2 5 6]);
+figure(1)
 plot([startpoints(1 ,:); endpoints(1 ,:)],[startpoints(2 ,:);...
     endpoints(2 ,:)] , 'b-');
 ylim([-1.5 1.5])
 axis equal
-subplot(2,4,3);
+title('Original Image')
+figure(2)
+subplot(2,2,1);
 plot([start1(1 ,:); end1(1 ,:)],[start1(2 ,:);...
     end1(2 ,:)] , 'b-');
 axis equal
-subplot(2,4,4);
+title ('H1: Similarity Tranformation')
+subplot(2,2,2);
 plot([start2(1 ,:); end2(1 ,:)],[start2(2 ,:);...
     end2(2 ,:)] , 'b-');
+title ('H2: Similarity Tranformation')
 axis equal
-subplot(2,4,7);
+subplot(2,2,3);
 plot([start3(1 ,:); end3(1 ,:)],[start3(2 ,:);...
     end3(2 ,:)] , 'b-');
+title ('H3: Affine Tranformation')
 axis equal
-subplot(2,4,8);
+subplot(2,2,4);
 plot([start4(1 ,:); end4(1 ,:)],[start4(2 ,:);...
     end4(2 ,:)] , 'b-');
 axis equal
+title ('H4: Similarity Tranformation')
