673e0a18bf054fdeb22de95b106f3218c8881b69
salamarxos <pipi_karipi@hotmail.com>
2018-11-20 15:49:48 +0100

    uptodate

--- assignment1data/Assignment_1_Q_2.m
+++ assignment1data/Assignment_1_Q_2.m
@@ -1,13 +1,18 @@
 load('compEx1.mat');
-two_D = pflat(x2D);
-three_D = pflat(x3D);
-
+for i=1:max(size(x2D))
+    two_D(:,i) = pflat(x2D(:,i));
+end
+for i=1:max(size(x3D))
+three_D(:,i) = pflat(x3D(:,i));
+end
 figure(1)
+subplot(1,2,1)
 plot(two_D(1,:),two_D(2,:),'o','MarkerSize',4,'MarkerEdgeColor','red',...
     'MarkerFaceColor','red')
 title('Homogeneous coordinates of x2D')
-
-figure(2)
+axis equal
+subplot(1,2,2)
 plot3(three_D(1,:),three_D(2,:),three_D(3,:),'o','MarkerSize',4,'MarkerEdgeColor','red',...
     'MarkerFaceColor','red')
 title('Homogeneous coordinates of x3D')
+axis equal
